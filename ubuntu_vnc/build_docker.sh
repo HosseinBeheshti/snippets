7295c68813d0d8b1bbc8bf570ebec8155165e22d
Hossein Beheshti <h.beheshti92@gmail.com>
2025-04-21 11:09:18 +0000

    Refactor Dockerfile and build script for Ubuntu VNC setup; remove entrypoint script and streamline password handling

--- ubuntu_vnc/build_docker.sh
+++ ubuntu_vnc/build_docker.sh
@@ -2,9 +2,6 @@
 mkdir ubuntu-vnc-l2tp
 cd ubuntu-vnc-l2tp
 
-wget https://raw.githubusercontent.com/hossenibeheshti/ubuntu-vnc-l2tp/main/Dockerfile
-wget https://raw.githubusercontent.com/hossenibeheshti/ubuntu-vnc-l2tp/main/entrypoint.sh
-
 docker build -t hossenibeheshti/ubuntu-vnc-l2tp:24.04 .
 
 docker push hossenibeheshti/ubuntu-vnc-l2tp:24.04
