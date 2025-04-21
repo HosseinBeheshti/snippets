6d270c7044e1d1763fbdcdc1ec0587892ef3ffac
Hossein Beheshti <h.beheshti92@gmail.com>
2025-04-21 11:16:13 +0000

    Add Dockerfile creation and snap installation to build script for Ubuntu VNC setup

--- ubuntu_vnc/build_docker.sh
+++ ubuntu_vnc/build_docker.sh
@@ -2,6 +2,10 @@
 mkdir ubuntu-vnc-l2tp
 cd ubuntu-vnc-l2tp
 
+vim Dockerfile
+
+snap install docker
+
 docker build -t hossenibeheshti/ubuntu-vnc-l2tp:24.04 .
 
 docker push hossenibeheshti/ubuntu-vnc-l2tp:24.04
