96ad71bb58b88bb43a96b721d199ae59a9e58e11
Hossein Beheshti <h.beheshti92@gmail.com>
2025-04-23 17:11:41 +0000

    deepseek answer :)

--- ubuntu_gui_l2tp_docker/startup.sh
+++ ubuntu_gui_l2tp_docker/startup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Start essential services
+mkdir -p /var/run/dbus
+dbus-uuidgen > /var/lib/dbus/machine-id
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+
 service dbus start
 service NetworkManager start
+
+# Wait for NetworkManager to start
+sleep 3
 
 # Copy Xauthority for root GUI access
 cp /home/docker/.Xauthority /root/.Xauthority 2>/dev/null || true
@@ -13,6 +20,10 @@ su - docker -c "vncserver :1 -geometry 1920x1080 -depth 24"
 
 # Grant root GUI access
 su - docker -c "xhost +SI:localuser:root"
+
+# Start polkit authentication agent
+export $(dbus-launch)
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
 # Keep container running
 tail -f /dev/null
